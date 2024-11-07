408cff506a324eb7bd57250de3a09529e5865383
Gayaneng <gsanjeewa1111@gmail.com>
2024-11-07 18:55:14 +0530

    add

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,20 @@
-console.log("hey")
+import express from 'express'
+import bodyParser  from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
 
+
+const app =  express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.post('/register' , (req , res) =>{
+    res.send({
+        message: `registered   ${req.body.email}`
+    })
+
+})
+
+app.listen(process.env.PORT || 8081)
